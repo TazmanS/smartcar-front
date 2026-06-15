e393c596c3c2165b0c7d0ba58127b859a69c78ea
Yevhen Shumilov <tazmans123@gmail.com>
2026-06-15 18:47:05 +0100

    add Dockerfile

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: apiUrl + "/api",
   timeout: 5000,
 });
--- src/api/services/status-service.ts
+++ src/api/services/status-service.ts
@@ -2,7 +2,7 @@ import { api } from '../client'
 import type { StatusResponse } from '../types/status-type';
 
 export const getStatusService = async (): Promise<StatusResponse> => {
-  const response = await api.get<StatusResponse>('/');
+  const response = await api.get<StatusResponse>('/car-status');
 
   return response.data;
 }
